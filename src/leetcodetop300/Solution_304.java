4edb2cde737b7db3a8f5906ccc04bc74af600df9
落雪无痕、 <3156813289@qq.com>
2021-03-02 09:03:02 +0800

    剑指offer

---
+++ src/leetcodetop300/Solution_304.java
@@ -0,0 +1,22 @@
+package leetcodetop300;
+
+/**
+ * @author: xuejing
+ * @date: 2021/03/02 8:59
+ * 304. 二维区域和检索 - 矩阵不可变
+ */
+public class Solution_304 {
+
+    // 方法一：使用一维数组前缀和，提前求出每一行数组的一维前缀和
+    // 结果就是n行数组前缀和
+    static class NumMatrix {
+
+        public NumMatrix(int[][] matrix) {
+
+        }
+
+        public int sumRegion(int row1, int col1, int row2, int col2) {
+
+        }
+    }
+}
---
+++ src/sword_finger_offer/Offer_57_II.java
@@ -0,0 +1,31 @@
+package sword_finger_offer;
+
+import org.w3c.dom.ls.LSException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: xuejing
+ * @date: 2021/02/25 8:56
+ * 剑指 Offer 57 - II. 和为s的连续正数序列
+ * 输入一个正整数 target ，输出所有和为 target 的连续正整数序列（至少含有两个数）。
+ * 序列内的数字由小到大排列，不同序列按照首个数字从小到大排列。
+ */
+public class Offer_57_II {
+
+    // 本题是寻找连续的整数和，并要求和为 target
+    // 所以可以使用滑动窗口，
+//    public int[][] findContinuousSequence(int target) {
+//        List<int[]> list = new ArrayList<int[]>();
+//        int start = 1;
+//        int end = 2;
+//        int sum = 0;
+//        while (end <= (target + 1) / 2) {
+//            if (sum == target) {
+//                list.add()
+//            }
+//        }
+//        return list.toArray(new int[list.size()][]);
+//    }
+}
